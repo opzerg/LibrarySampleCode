f8977c2093b9bae6e1313acdb2a67d731d3b0324
용국 최 <opzerg9378@gmail.com>
2021-07-16 00:10:11 +0900

    feature: .net framework 4.5 unit test project add update: .net 5 unit test project change name feature: sample code write 'DetailMessageBoxTest'

---
+++ SampleCode4.5/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SampleCode4.5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleCode4.5")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("54bf2ccf-61c5-4328-9f7a-65cf0b33f051")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SampleCode4.5/YonggukUtilsTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+using YonggukUtils;
+
+namespace SampleCode4._5
+{
+    [TestClass]
+    public class YonggukUtilsTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void DetailMessageBoxTest()
+        {
+            using (var form = MessageBoxUtils.CreateDetailMessageBox("제목", "내용", "자세히"))
+            {
+                TestContext.WriteLine($"{form.ShowDialog()}");
+            }
+        }
+    }
+}
