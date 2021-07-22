41db6b17d6a2b9f8a6369b176826d6fb01381799
용국 최 <opzerg9378@gmail.com>
2021-07-22 23:30:01 +0900

    feature: add CommandLineParser and define CLIOption.cs test: wrte to CommandLineHelper test code

---
+++ MyModel/Command/CLIOption.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyModel.Command
+{
+    
+    public class CLIOption
+    {
+        [Option('p', "path", Required = true, HelpText = "원본 파일 경로")]
+        public string Path { get; set; }
+        [Option('d', "dest", Required = true, HelpText = "복사 파일 경로")]
+        public string DestPath { get; set; }
+        [Option('o', "overwrite", HelpText = "덮어쓰기")]
+        public bool Overwrite { get; set; }
+    }
+}
---
+++ SampleCode5.0/Command/CommandTest.cs
@@ -0,0 +1,33 @@
+﻿using CommandLine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyModel.Command;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode5._0
+{
+    [TestClass]
+    public class CommandTest
+    {
+        public string TestFile = "rip.jpg";
+        [TestMethod]
+        public void CLIOptionTest()
+        {
+            string[] args = new[] { "-p", Path.Combine(Environment.CurrentDirectory, TestFile), "-d", Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), TestFile), "-o" };
+
+            Parser.Default.ParseArguments<CLIOption>(args).WithParsed(cliOption =>
+            {
+                File.Copy(cliOption.Path, cliOption.DestPath, cliOption.Overwrite);
+            }).WithNotParsed(e =>
+            {
+                Assert.Fail();
+            });
+
+
+        }
+    }
+}
