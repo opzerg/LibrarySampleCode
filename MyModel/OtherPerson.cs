ca10abd049a39ea1dc87040f10abad01fa6679bf
용국 최 <opzerg9378@gmail.com>
2021-07-27 08:27:48 +0900

    feature: add nuget package AutoMapper feature: add OtherPerson.cs & write test sample code

---
+++ MyModel/OtherPerson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyModel
+{
+    public class OtherPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"이름: {Name} 나이: {Age} 이메일: {Email}";
+        }
+    }
+}
---
+++ SampleCode5.0/Mapper/AutoMapperTest.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode5._0
+{
+    [TestClass]
+    public class AutoMapperTest
+    {
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+        public void PersonMapperTest()
+        {
+            Person person = new Person()
+            {
+                Age = 29,
+                Gender = true,
+                Name = "최용국"
+            };
+
+            OtherPerson otherPerson = new OtherPerson()
+            {
+                Email = "opzerg9378@gmail.com"
+            };
+
+            Mapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Person, OtherPerson>();
+            }));
+
+            TestContext.WriteLine($"{mapper.Map(person, otherPerson)}");
+        }
+    }
+}
