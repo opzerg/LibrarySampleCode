37da0da239d0d320f5ba4d5607e32d10c60a0d0b
용국 최 <opzerg9378@gmail.com>
2021-08-01 23:57:31 +0900

    feature: add IPCTest.cs
    todo: write to sample code

---
+++ SampleCode5.0/IPC/IPCTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode5._0
+{
+    
+    [TestClass]
+    public class IPCTest
+    {
+        public TestContext TestContext { get; set; }
+        string PipeName => "testpipe";
+        [TestMethod]
+        public void NamedPipeTest()
+        {
+            
+        }
+
+        async void PipeServer()
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                NamedPipeServerStream pipeServer = new NamedPipeServerStream(PipeName);
+
+                pipeServer.WaitForConnection();
+
+
+
+            });
+
+            TestContext.WriteLine("end pipe server");
+        }
+    }
+}
