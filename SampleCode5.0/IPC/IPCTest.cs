715c7f672147fd01cdc7bcae6f1b750bfae76dd4
용국 최 <opzerg9378@gmail.com>
2021-08-02 00:21:15 +0900

    update: namedPipe write sample code

--- SampleCode5.0/IPC/IPCTest.cs
+++ SampleCode5.0/IPC/IPCTest.cs
@@ -13,14 +13,16 @@ namespace SampleCode5._0
     public class IPCTest
     {
         public TestContext TestContext { get; set; }
+        Encoding Encoding = Encoding.UTF8;
         string PipeName => "testpipe";
         [TestMethod]
         public void NamedPipeTest()
         {
-            
+            RunPipeServer();
+            RunPipeClient();
         }
 
-        async void PipeServer()
+        async void RunPipeServer()
         {
             await Task.Factory.StartNew(() =>
             {
@@ -28,11 +30,38 @@ namespace SampleCode5._0
 
                 pipeServer.WaitForConnection();
 
+                // read length
+                var len = pipeServer.ReadByte() * 64;
+                len += pipeServer.ReadByte();
+                byte[] inBuffer = new byte[len];
+                var rlen = pipeServer.Read(inBuffer, 0, len);
 
 
+                Console.WriteLine($"receive server: {Encoding.GetString(inBuffer, 0, rlen)}");
+                pipeServer.Close();
             });
 
             TestContext.WriteLine("end pipe server");
+        }
+
+        void RunPipeClient()
+        {
+            var pipeClient = new NamedPipeClientStream(".", PipeName, PipeDirection.InOut, PipeOptions.Asynchronous);
+
+            pipeClient.Connect();
+
+            string message = "i am client";
+            
+            byte[] outBuffer = Encoding.GetBytes(message);
+            int len = outBuffer.Length;
+
+            pipeClient.WriteByte((byte)(len / 64));
+            pipeClient.WriteByte((byte)(len * 64));
+            pipeClient.Write(outBuffer, 0, len);
+            pipeClient.Flush();
+
+            pipeClient.Close();
+            TestContext.WriteLine("client: bye bye~");
         }
     }
 }
